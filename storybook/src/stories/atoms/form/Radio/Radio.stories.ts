e0270e1ea92ca1949c06bfb6677f3b7204f82f59
Connor van Spronssen <connor.van.spronssen@iodigital.com>
2024-07-10 08:01:27 +0200

    Add a basic Radio component

---
+++ storybook/src/stories/atoms/form/Radio/Radio.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import type { ComponentProps } from 'vue-component-type-helpers'
+import Radio from './Radio.vue'
+
+type Props = ComponentProps<typeof Radio>
+
+const meta: Meta<Props> = {
+  title: 'Atoms/Form/Radio',
+  component: Radio,
+  tags: ['autodocs'],
+  args: {},
+  parameters: {
+    slots: {
+      default: {},
+    },
+  },
+}
+
+function getOptions(number: number) {
+  const options = []
+
+  for (let i = 0; i < number; i++) {
+    options.push({
+      label: `Option ${i + 1}`,
+    })
+  }
+
+  return options
+}
+
+export default meta
+type Story = StoryObj<Props>
+
+export const Default: Story = {
+  args: {
+    name: 'default',
+    options: getOptions(3),
+    required: false,
+  },
+}
---
+++ storybook/src/stories/atoms/form/Radio/Radio.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge'
+import type { HTMLAttributes } from 'vue'
+import Label from '../Label/Label.vue'
+
+const props = withDefaults(defineProps<{
+  class?: HTMLAttributes['class']
+  wrapperClass?: HTMLAttributes['class']
+  name: string
+  options: {
+    label: string
+  }[]
+  required?: boolean
+}>(), {
+})
+</script>
+
+<template>
+  <div
+    class="flex flex-col gap-2"
+  >
+    <div
+      v-for="(option, index) of props.options"
+      :key="option.label"
+      :class="twMerge(
+        'flex items-center gap-2',
+        props.wrapperClass,
+      )"
+    >
+      <input
+        :id="`${option.label.toLocaleLowerCase()}${index}`"
+        :class="twMerge(
+          '',
+          props.class,
+        )"
+        :name="props.name"
+        type="radio"
+      >
+
+      <Label
+        :for="`${option.label.toLocaleLowerCase()}${index}`"
+      >
+        {{ option.label }}
+      </Label>
+    </div>
+  </div>
+</template>
