ef9e8fefc66849106475f4025872ee9816a75ae3
Connor van Spronssen <connor.van.spronssen@iodigital.com>
2024-07-12 17:43:56 +0200

    Add a Container component

---
+++ storybook/src/stories/atoms/containers/Container/Container.stories.ts
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import type { ComponentProps } from 'vue-component-type-helpers'
+import Component from './Container.vue'
+
+type Props = ComponentProps<typeof Component> & { default: string }
+
+const meta: Meta<Props> = {
+  title: 'Atoms/Containers/Container',
+  component: Component,
+  tags: ['autodocs'],
+  args: {
+    class: '[background-image:repeating-linear-gradient(-55deg,#0002,#0002_20px,#ffb10122_20px,#ffb10122_40px)]',
+    default: 'Container',
+    type: 'regular',
+  },
+  argTypes: {
+    type: {
+      control: 'inline-radio',
+      options: ['regular', 'wide', 'small', 'full'],
+    },
+  },
+  parameters: {
+    slots: {
+      default: {},
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<Props>
+
+export const Default: Story = {
+  args: {},
+}
+
+export const Wide: Story = {
+  args: {
+    type: 'wide',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    type: 'small',
+  },
+}
+
+export const Full: Story = {
+  args: {
+    type: 'full',
+  },
+}
---
+++ storybook/src/stories/atoms/containers/Container/Container.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge'
+import { type HTMLAttributes, computed } from 'vue'
+
+const props = withDefaults(defineProps<{
+  class?: HTMLAttributes['class']
+  type: 'regular' | 'wide' | 'small' | 'full'
+}>(), {
+  type: 'regular',
+})
+
+const width = computed(() => {
+  switch (props.type) {
+    case 'wide':
+      return 'max-w-7xl'
+    case 'small':
+      return 'max-w-5xl'
+    case 'full':
+      return ''
+    default:
+      return 'max-w-6xl'
+  }
+})
+</script>
+
+<template>
+  <div
+    :class="twMerge(
+      'mx-auto p-8',
+      width,
+      props.class,
+    )"
+  >
+    <slot />
+  </div>
+</template>
